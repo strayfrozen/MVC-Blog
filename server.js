71da9889af5dc9022efaaf336a329c38adc504a0
Stray <strayfrozen@yahoo.com>
2022-01-18 16:20:04 -0600

    added handel bars connected it to server.js and created main.handlebars

--- server.js
+++ server.js
@@ -1,3 +1,35 @@
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*GIVEN a CMS-style blog site
 WHEN I visit the site for the first time
 THEN I am presented with the homepage, which includes existing blog posts if any have been posted; navigation links for the homepage and the dashboard; and the option to log in
