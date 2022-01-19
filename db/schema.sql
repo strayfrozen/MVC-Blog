04dcafdec951c2afac99048922fd49de80aea5a3
Stray <strayfrozen@yahoo.com>
2022-01-19 15:09:55 -0600

    set up barebones user model

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,3 @@
-DROP DATABASE IF EXISTS mvc_db;
+DROP DATABASE IF EXISTS just_tech_news_db;
 
-CREATE DATABASE mvc_db;
+CREATE DATABASE just_tech_news_db;
---
+++ models/User.js
@@ -0,0 +1,19 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+  {
+    
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
