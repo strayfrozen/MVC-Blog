5dd64b007a2b34a562d0e32abb7798621935e99d
Stray <strayfrozen@yahoo.com>
2022-01-22 12:43:45 -0600

    created a basic comment model

---
+++ models/Comment.js
@@ -0,0 +1,18 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+   
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+  }
+);
+
+module.exports = Comment;
