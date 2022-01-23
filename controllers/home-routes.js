c49f508d3250005c7be2210289f51cfd7b0c6b06
Stray <strayfrozen@yahoo.com>
2022-01-23 11:14:58 -0600

    created public folder and login.js file to add front end functuality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
-router.get('/', (req, res) => {
+router.get('/homepage', (req, res) => {
   Post.findAll({
     attributes: [
       'id',
