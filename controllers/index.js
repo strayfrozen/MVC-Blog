24ed20f57ba1e39e86e35a4dd91c703ec595ccf2
Stray <strayfrozen@yahoo.com>
2022-01-23 12:40:27 -0600

    added dashboard-routes file created get route

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 
 const homeRoutes = require('./home-routes.js');
-
+const dashboardRoutes = require('./dashboard-routes.js');
 
 
 router.use('/', homeRoutes);
-
+router.use('/dashboard', dashboardRoutes);
 
 
 module.exports = router;
