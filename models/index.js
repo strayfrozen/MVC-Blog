891feaefc3b8bec9141357809acb5757f545554f
Stray <strayfrozen@yahoo.com>
2022-01-23 09:13:46 -0600

    created user and post model associations

--- models/index.js
+++ models/index.js
@@ -1,7 +1,17 @@
 const User = require('./User');
 const Comment = require('./Comment');
+const Post = require("./Post");
 
 
-module.exports = { User, Comment };
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+  Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+
+
+module.exports = { User, Comment, Post };
 
 
