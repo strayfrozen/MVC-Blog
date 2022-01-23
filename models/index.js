f0574803583f2b2ea86ea90f368f4f04910d0d76
Stray <strayfrozen@yahoo.com>
2022-01-23 10:30:00 -0600

    updated associations in index model

--- models/index.js
+++ models/index.js
@@ -3,14 +3,31 @@ const Comment = require('./Comment');
 const Post = require("./Post");
 
 
+// create associations
 User.hasMany(Post, {
     foreignKey: 'user_id'
   });
-
+  
   Post.belongsTo(User, {
-    foreignKey: 'user_id',
+    foreignKey: 'user_id'
   });
-
+  
+  
+  Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
 
 module.exports = { User, Comment, Post };
 
