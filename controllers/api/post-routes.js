a3c6b9b872baaf3f5a8779101c4949d2ca0aed73
Stray <strayfrozen@yahoo.com>
2022-01-23 09:22:08 -0600

    created put and delete and update get all post route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,14 +4,15 @@ const { Post, User } = require('../../models');
 
 router.get('/', (req, res) => {
     Post.findAll({
-      attributes: ['id', 'post_url', 'title', 'created_at'],
-      include: [
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
-    })
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        order: [['created_at', 'DESC']], 
+        include: [
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
       .then(dbPostData => res.json(dbPostData))
       .catch(err => {
         console.log(err);
@@ -60,5 +61,48 @@ router.get('/', (req, res) => {
       });
   });
 
+
+  router.put('/:id', (req, res) => {
+    Post.update(
+      {
+        title: req.body.title
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  router.delete('/:id', (req, res) => {
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
   module.exports = router;
