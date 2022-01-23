8a0b2c86184b79aaa0e66f01a27862a23c48ce12
Stray <strayfrozen@yahoo.com>
2022-01-23 09:19:20 -0600

    created a get by id and a post route for post-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
+
 const commentRoutes = require('./comment-routes');
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
 router.use('/comments', commentRoutes);
+router.use('/posts', postRoutes);
 
 
 module.exports = router;
